1d662b178de354644f53e02c7fc6861ed0b390ed
Wade <gdwade@weathertop.middlebury.edu>
2024-12-02 20:48:57 -0500

    Code simplified, optional args should work now. Pipe still not started.

--- mysh.c
+++ mysh.c
@@ -26,91 +26,81 @@ char * strip_new_line (char * input) {
 int main (int argc, char * argv[]) {
     printf("Mysh: ");
     char cmd_buff[4096];
+
     while (fgets(cmd_buff, sizeof(cmd_buff), stdin) != NULL) {
+        char ** tokens = malloc(10*sizeof(char *));
         char *token = strtok(cmd_buff, " ");
-        if (strcmp(token, "exit\n") == 0 || strcmp(token, "exit") == 0) {
-            // First word is "exit" (if no new line, then exit is not the only word in the cmd)
+        char * first_token = strip_new_line(token);
+        int op_flag = 0;
+        int counter = 0;
+
+        if (strcmp(first_token, "exit") == 0) {
+            free(tokens);
+            free(first_token);
             return 0;
         }
-        int op_flag = 0;
-        //int counter = 0;
-        char * cmd = token;
-        // char * opts[25]; 
+        tokens[counter++] = token; // The ++ is on purpose!
+        free(first_token);
         while ((token = strtok(NULL, " ")) != NULL) {
             int initial_fd;
             pid_t child;
-            switch (op_flag) {
-                // CASES 1,2,3 are the same except for the flag that goes into open. Should just combine them...
-                case 1:
-                    puts("Handle input redirect");
-                    child = fork(); // ERROR CHECK ME?
-                    if (child == 0) {
-                        if ((initial_fd = open(strip_new_line(token), O_RDONLY)) < 0) {
-                        perror("open");
-                        exit(-1);
-                    };
-                    dup2(initial_fd, STDIN_FILENO); // ERROR CHECK ME!
-                    close(initial_fd);
-                    execl(cmd,cmd,NULL); //ERROR CHECK ME!
-                    exit(0);
-                    }else{
-                        wait(NULL);
-                        break;
-                    } 
-                case 2:
-                    // Handle output redirect (trunc)
-                    puts("Handle output redirect");
-                    child = fork(); // ERROR CHECK ME?
-                    if (child == 0){
-                        if ((initial_fd = open(strip_new_line(token), O_WRONLY | O_TRUNC)) < 0) {
-                            perror("open");
-                            exit(-1);
-                        }
-                        dup2(initial_fd, fileno(stdout)); // ERROR CHECK ME!
-                        close(initial_fd);
-                        execl(cmd,cmd,NULL); //ERROR CHECK ME!
-                        exit(0);
-                    }else{
-                        wait(NULL);
-                        break;
-                    }
-                case 3:
-                    child = fork(); // ERROR CHECK ME?
-                    if (child == 0){
-                        if ((initial_fd = open(strip_new_line(token), O_WRONLY | O_APPEND)) < 0) {
-                            perror("open");
-                            exit(-1);
-                        }
-                        dup2(initial_fd, fileno(stdout)); // ERROR CHECK ME!
-                        close(initial_fd);
-                        execl(cmd,cmd,NULL); //ERROR CHECK ME!
-                        exit(0);
-                    }else{
-                        wait(NULL);
-                        break;
-                    }
-                    break;
-                case 4:
-                    // Handle pipe
-                    break;
-                default:
-                    break;
+
+            if (op_flag == 4) {
+                // HANDLE PIPE
+                puts("Pipe failed. Too bad thats not implemented yet.");
+                return -1;
             }
-            /* NOTE: ++ returns value before it increments, so technically this does opts[counter] then increments counter */
-            //printf("%s\n", token);
+            if (op_flag != 0) {
+                // HANDLE ALL  OTHER OPS
+                int open_mode;
+                int new_fd = STDOUT_FILENO; // Defualt to dup STDOUT
+                switch (op_flag) {
+                    case 1: 
+                        open_mode = O_RDONLY;
+                        new_fd = STDIN_FILENO; // Special case, dup STDIN
+                        break;
+                    case 2:
+                        open_mode = O_WRONLY | O_TRUNC;
+                        break;
+                    default:
+                        open_mode = O_WRONLY | O_APPEND;
+                        break;
+                }
+                child = fork(); // ERROR CHECK ME?
+                char * file_name = strip_new_line(token);
+                if (child == 0) {
+                    if ((initial_fd = open(file_name, open_mode)) < 0) {
+                    perror("open");
+                    exit(-1);
+                };
+                dup2(initial_fd, new_fd); // ERROR CHECK ME!
+                close(initial_fd); // ERROR CHECK ME!
+                execv(tokens[0], tokens); //ERROR CHECK ME!
+                free(file_name);
+                exit(0);
+                }else{
+                    wait(NULL);
+                    break;
+                } 
+            }
             if (strcmp(token,"<") == 0) {
                 op_flag = 1; // In redirect
-            }
-            if (strcmp(token,">") == 0) {
+            } else if (strcmp(token,">") == 0) {
                 op_flag = 2; // Out redirect
-            }
-            if (strcmp(token,">>") == 0) {
+            } else if (strcmp(token,">>") == 0) {
                 op_flag = 3; // Out redirect special
-            }
-            if (strcmp(token,"|") == 0) {
+            } else if (strcmp(token,"|") == 0) {
                 op_flag = 4; // Pipe
+            }else {
+                // Save token into tokens then increment
+                tokens[counter++] = token;
+                if (counter % 10 == 0) {
+                    // Resize if there are somehow more than 10 optional args
+                    tokens = realloc(tokens,counter + 10);
+                }
             }
         }
+        free(tokens);
         printf("Mysh: ");
     }
     return 0;
