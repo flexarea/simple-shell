1cf33c24422f55277b6283dd8eba5bc5fb6915ff
Wade <gdwade@weathertop.middlebury.edu>
2024-12-03 11:02:11 -0500

    started working on pipe

--- mysh.c
+++ mysh.c
@@ -32,17 +32,21 @@ void free_tokens (char ** tokens, int length) {
 int main (int argc, char * argv[]) {
     printf("Mysh: ");
     char cmd_buff[4096];
-
+    // Cursed pointers ahead
+    char *** pipe_tokens = malloc(2 * sizeof(char **)); // ERROR CHECK ME!!!!
+    int num_pipes = 0;
     while (fgets(cmd_buff, sizeof(cmd_buff), stdin) != NULL) {
-        char ** tokens = malloc(10*sizeof(char *));
+        // Commands have a max of 10 args, so total of 11 b/c cmd name counts?
+        char ** tokens = malloc(11 * sizeof(char *));
         char *token;
-        if ((token = strtok(cmd_buff, " ")) == NULL){
+        int op_flag = 0;
+        int counter = 0;
+        // Initial Setup
+        if ((token = strtok(cmd_buff, " ")) == NULL) {
             return -1;
         }
         token = strip_new_line(token);
-        int op_flag = 0;
-        int counter = 0;
-
+        // Handle exit
         if (strcmp(token, "exit") == 0) {
             free(tokens);
             return 0;
@@ -51,13 +55,7 @@ int main (int argc, char * argv[]) {
         while ((token = strtok(NULL, " ")) != NULL) {
             int initial_fd;
             pid_t child;
-
-            if (op_flag == 4) {
-                // HANDLE PIPE
-                puts("Pipe failed. Too bad thats not implemented yet.");
-                return -1;
-            }
-            if (op_flag != 0) {
+            if (op_flag != 0 && op_flag != 4) {
                 // HANDLE ALL  OTHER OPS
                 int open_mode;
                 int new_fd = STDOUT_FILENO; // Defualt to dup STDOUT
@@ -84,11 +82,19 @@ int main (int argc, char * argv[]) {
                 close(initial_fd); // ERROR CHECK ME!
                 execv(tokens[0], tokens); //ERROR CHECK ME!
                 exit(0);
-                }else{
+                } else {
                     wait(NULL);
                     break;
                 } 
             }
+            if (op_flag == 4) {
+                // HANDLE PIPE
+                pipe_tokens[num_pipes] = tokens;
+                tokens = malloc(11 * sizeof(char *)); // ERROR CHECK ME!
+                num_pipes += 1;
+                counter = 0; // RESET counter
+            }
+
             if (strcmp(token,"<") == 0) {
                 op_flag = 1; // In redirect
             } else if (strcmp(token,">") == 0) {
@@ -97,13 +103,24 @@ int main (int argc, char * argv[]) {
                 op_flag = 3; // Out redirect special
             } else if (strcmp(token,"|") == 0) {
                 op_flag = 4; // Pipe
-            }else {
-                // Save token into tokens then increment
+            } else {
+                // Save (stripped) token into tokens then increment
                 tokens[counter++] = strip_new_line(token);
+                
                 if (counter % 10 == 0) {
                     // Resize if there are somehow more than 10 optional args
+                    // Technically not needed, but it can't hurt right?
                     tokens = realloc(tokens,counter + 10);
                 }
+            }
+        }
+        // Loop has ended, we need to handle pipe here for whatever the last argument is.
+        // I need a list of toekns. 
+        if (op_flag == 4) {
+            for (int i = 0; i < num_pipes, i++) {
+                //Process and fork
+                // Inside child:
+                //    handle_pipe(_, _, _)
             }
         }
         if (op_flag == 0) {
@@ -117,9 +134,23 @@ int main (int argc, char * argv[]) {
                  wait(NULL);
             }
         }
-        free_tokens(tokens,counter);
+        free_tokens(tokens, counter);
         op_flag = 0;
         printf("Mysh: ");
     }
     return 0;
+}
+
+void * handle_pipe(int location, int pipefd[2], char ** tokens) {
+    /*
+    location = 0 -> first cmd:
+        - dup2 stdout -> pipefd[0]
+    location = 1 -> any other cmd:
+        - dup2 stdout -> pipefd[0]
+        - dup2 stdin -> pipefd[1]
+    location = 2 -> last cmd:
+        - dup2 stdin -> pipefd[1]
+    ALL:
+    execv(tokens[0], tokens);
+    */
 }
