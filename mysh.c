6118a06c4a10d0eb823838e6568c4a994f9ad09f
Wade <gdwade@weathertop.middlebury.edu>
2024-12-03 23:15:04 -0500

    I dont know whats wrong

--- mysh.c
+++ mysh.c
@@ -8,23 +8,39 @@
 #define LAST_CMD 2
 #define FIRST_CMD 0
 
+struct p_tkn {
+    char ** tkns;
+    int len;
+};
+
 void handle_pipe(int type, int fd_1, int fd_2, char ** tokens);
 
 char * strip_new_line (char * input) {
     char c = *input;
-    char *out = malloc(100);
+    char *out;
+    if ((out = malloc(100)) == NULL) {
+        perror("malloc");
+        exit(-1);
+    }
     int counter = 0;
     // Iterate until we find null char or new line
     while (c != '\n' && c != '\0') {
         if (counter % 100 == 0) {
-            out = (char *) realloc(out, counter + 100);
+            if ((out = (char *) realloc(out, counter + 100)) == NULL){
+                perror("realloc");
+                exit(-1);
+            }
         }
-        out[counter++] = c;
+        out[counter] = c;
+        counter += 1;
         c = input[counter];
     }
     // Tack on null char
     out[counter] = '\0';
-    out = (char *) realloc(out, counter);
+    if ((out = (char *) realloc(out, counter)) == NULL){
+        perror("realloc");
+        exit(-1);
+    }
     return out;
 }
 void free_tokens (char ** tokens, int length) {
@@ -37,18 +53,23 @@ void free_tokens (char ** tokens, int length) {
 int main (int argc, char * argv[]) {
     printf("Mysh: ");
     char cmd_buff[4096];
-    // Cursed pointers ahead
-    char *** pipe_tokens;
-    if ((pipe_tokens = malloc(2 * sizeof(char **))) == NULL) {
-        return -1;
-    }
-    int num_pipes = 0;
     while (fgets(cmd_buff, sizeof(cmd_buff), stdin) != NULL) {
-        // Commands have a max of 10 args, so total of 11 b/c cmd name counts?
-        char ** tokens = malloc(11 * sizeof(char *));
+        // Commands have a max of 10 args, so total of 11 b/c cmd name counts
+        char ** tokens;
+        if ((tokens = malloc(11 * sizeof(char *))) == NULL) {
+            return -1;
+        }
+        // An array of tokens (defualt size 2) realloc'd for more later in needed (not done)
+        struct p_tkn * pipe_tkns;
+        // Allocate 2 p_tkn's' by defualt since thats the min.
+        int pipe_tkns_len = 2;
+        if ((pipe_tkns = malloc(2 * sizeof(struct p_tkn))) == NULL) {
+            return -1;
+        }
         char *token;
         int op_flag = 0;
         int counter = 0;
+        int pipe_counter = 0;
         // Initial Setup
         if ((token = strtok(cmd_buff, " ")) == NULL) {
             return -1;
@@ -63,7 +84,8 @@ int main (int argc, char * argv[]) {
             free(tokens);
             return 0;
         }
-        tokens[counter++] = token; // The ++ is on purpose!
+        tokens[counter] = token;
+        counter += 1;
         while ((token = strtok(NULL, " ")) != NULL) {
             int initial_fd;
             pid_t child;
@@ -93,7 +115,7 @@ int main (int argc, char * argv[]) {
                         perror("open");
                         exit(-1);
                     };
-                free(file_name);
+                //free(file_name);
                 dup2(initial_fd, new_fd); // ERROR CHECK ME!
                 close(initial_fd); // ERROR CHECK ME!
                 if (execvp(tokens[0], tokens) == -1){
@@ -106,15 +128,6 @@ int main (int argc, char * argv[]) {
                     break;
                 } 
             }
-            if (op_flag == 4) {
-                pipe_tokens[num_pipes] = tokens;
-                if ((tokens = malloc(11 * sizeof(char *))) == NULL) {
-                    return -1;
-                }
-                num_pipes += 1;
-                counter = 0; // RESET counter
-            }
-
             if (strcmp(token,"<") == 0) {
                 op_flag = 1; // In redirect
             } else if (strcmp(token,">") == 0) {
@@ -123,49 +136,31 @@ int main (int argc, char * argv[]) {
                 op_flag = 3; // Out redirect special
             } else if (strcmp(token,"|") == 0) {
                 op_flag = 4; // Pipe
+                if (pipe_counter > pipe_tkns_len) {
+                    // Allocate two extra p_tkn's
+                    puts("extending");
+                    pipe_tkns_len = pipe_counter + 1;
+                    pipe_tkns = realloc(pipe_tkns, pipe_tkns_len * sizeof(struct p_tkn));
+                }
+                pipe_tkns[pipe_counter].tkns = tokens;
+                pipe_tkns[pipe_counter].len = counter - 1; 
+                counter = 0;
+                pipe_counter += 1;
+                
             } else {
                 // Save (stripped) token into tokens then increment
-                tokens[counter++] = strip_new_line(token);
+                tokens[counter] = strip_new_line(token);
+                counter += 1;
                 if (counter % 10 == 0) {
                     // Resize if there are somehow more than 10 optional args
                     // Technically not needed, but it can't hurt right?
-                    tokens = realloc(tokens,counter + 10);
+                    tokens = realloc(tokens, (counter + 10) * sizeof(char *));
                 }
             }
         }
-        pipe_tokens[num_pipes] = tokens;
         if (op_flag == 4) {
-            int pipefd[2];
-            for (int i = 0; i <= num_pipes; i++) {
-                // Process and fork
-                char ** tok = pipe_tokens[i];
-                if (i % 2 == 0) {
-                    if (pipe(pipefd) == -1) {
-                        perror("pipe");
-                        return(-1);
-                    }
-                }
-                // fixed weird seg fualt thing (I think)
-                int save_0 = pipefd[0];
-                int save_1 = pipefd[1];
-                pid_t child;
-                if ((child = fork()) == -1) {
-                    perror("fork");
-                    return(-1);
-                }
-                if (child == 0) {
-                    int type = 1;
-                    if (i == 0) {
-                        type = 0;
-                    }
-                    if (i == num_pipes) {
-                        type = 2;
-                    }
-                    handle_pipe(type, save_1, save_0, tok);
-                }
-            }
-            for (int i = 0; i <= num_pipes; i++) {
-                wait(NULL);
+            for (int i = 0; i < pipe_counter; i ++) {
+                puts("pipe");
             }
         }
         if (op_flag == 0) {
@@ -184,7 +179,7 @@ int main (int argc, char * argv[]) {
                  wait(NULL);
             }
         }
-        free_tokens(tokens, counter);
+        //free_tokens(tokens, counter);
         op_flag = 0;
         printf("Mysh: ");
     }
